3a7ffa2177ffd09704d47e15ca8c881e40f4af4c
Lehelbalint <balint.lehel@student.ms.sapientia.ro>
2021-06-04 15:13:33 +0300

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/3Projekt.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "3Projekt.exe"
+  "3Projekt.exe.manifest"
+  "3Projekt.pdb"
+  "CMakeFiles/3Projekt.dir/main.c.obj"
+  "lib3Projekt.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/3Projekt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
